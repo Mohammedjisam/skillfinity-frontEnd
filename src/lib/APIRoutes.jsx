2f2257f42fc6cc2f87ed2564fefc1d5b9cb19a1e
Mohammed Jisam <mohammedjisamtp@gmail.com>
2024-12-03 20:04:07 +0530

    redux store updation

--- src/lib/APIRoutes.jsx
+++ src/lib/APIRoutes.jsx
@@ -1,4 +1,4 @@
-export const host = 'http://localhost:3000'
+export const host = 'http://13.234.124.12'
 export const allUsersRoute =`${host}/tutor/getStudents`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
 export const recieveMessageRoute =` ${host}/api/messages/getmsg`;
