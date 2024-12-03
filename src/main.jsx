ba669befa7743382a323b55dd2045ca28fba4b7b
Mohammed Jisam <mohammedjisamtp@gmail.com>
2024-12-03 19:30:26 +0530

    redux store updation

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'; 
-import store from './redux/store';
+import store from './redux/Store.jsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
