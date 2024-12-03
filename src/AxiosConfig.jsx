b159676a7968e93fac8037bb054d3592e89e9168
Mohammed Jisam <mohammedjisamtp@gmail.com>
2024-12-03 20:03:06 +0530

    redux store updation

--- src/AxiosConfig.jsx
+++ src/AxiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://13.234.124.12",
   withCredentials: true,
   timeout: 10000,
   });
